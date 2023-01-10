bab5e77c659db314debc93e11f2c2f06284d5336
Seixas007 <jorgesantosseixas@gmail.com>
2023-01-10 12:30:41 -0300

    alteracao nos dias da funcao calculo

--- signos.py
+++ signos.py
@@ -42,7 +42,7 @@ def verificador(dia, mes=None):
     # faz uma verificacao se o mes digitado está na lista da funcao meses()
     if mes not in m:
         print('\nmês de nascimento invalido')
-        print('Lista de mêses validos abaixo\n')
+        print('Lista de meses validos abaixo\n')
 
         # se não estiver na funcao meses()
         # ele mostra a lista de mês validos e encerra o programa
@@ -64,7 +64,7 @@ def verificador(dia, mes=None):
         else:
             return False
 
-def nascimento():
+def main():
     
     print ("Olá, gostaria de saber qual é o seu signo? ")
 
@@ -90,33 +90,33 @@ def nascimento():
     except:
         pass
     
-# calculo recebe da função nascimento() o dia e mês do usuario
+# calculo recebe da função main() o dia e mês do usuario
 def calculo(dia, mes):
     m = meses()
-    if dia>=20 and dia<=31 and mes==m[0]:
-        formatacao(dia, mes, "aries")
-    elif dia>=20 and dia<=30 and mes==m[1] or dia>=1 and dia<=20 and mes==m[2]:
-        formatacao(dia, mes, "touro")
+    if dia>=20 and dia<=31 and mes==m[0] or dia>=1 and dia<=18 and mes==m[1]:
+        formatacao(dia, mes, "Aries")
+    elif dia>=19 and dia<=30 and mes==m[1] or dia>=1 and dia<=19 and mes==m[2]:
+        formatacao(dia, mes, "Touro")
     elif dia>=20 and dia<=31 and mes==m[2] or dia>=1 and dia<=20 and mes==m[3]:
-        formatacao(dia, mes, "gemeos")
+        formatacao(dia, mes, "Gemeos")
     elif dia>=21 and dia<=30 and mes==m[3] or dia>=1 and dia<=21 and mes==m[4]:
-        formatacao(dia, mes, "cancer")
+        formatacao(dia, mes, "Cancer")
     elif dia>=22 and dia<=31 and mes==m[4] or dia>=1 and dia<=22 and mes==m[5]:
-        formatacao(dia, mes, "leão")
-    elif dia>=23 and dia<=31 and mes==m[5] or dia>=1 and dia<=22 and mes==m[6]:
-        formatacao(dia, mes, "virgem")
-    elif dia>=23 and dia<=30 and mes==m[6] or dia>=1 and dia<=22 and mes==m[7]:
-        formatacao(dia, mes, "libra")
-    elif dia>=23 and dia<=31 and mes==m[7] or dia>=1 and dia<=22 and mes==m[8]:
-        formatacao(dia, mes, "escorpião")
-    elif dia>=23 and dia<=30 and mes==m[8] or dia>=1 and dia<=23 and mes==m[9]:
-        formatacao(dia, mes, "sagitario")
-    elif dia>=23 and dia<=31 and mes==m[9] or dia>=1 and dia<=20 and mes==m[10]:
-        formatacao(dia, mes, "capricornio")
-    elif dia>=21 and dia<=31 and mes==m[10] or dia>=1 and dia<=19 and mes==m[11]:
-        formatacao(dia, mes, "aquario")
-    elif dia>=20 and dia<=29 and mes==m[11] or dia>=1 and dia<=19 and mes==m[0]:
-        formatacao(dia, mes, "peixes")
+        formatacao(dia, mes, "Leão")
+    elif dia>=23 and dia<=31 and mes==m[5] or dia>=1 and dia<=21 and mes==m[6]:
+        formatacao(dia, mes, "Virgem")
+    elif dia>=22 and dia<=30 and mes==m[6] or dia>=1 and dia<=22 and mes==m[7]:
+        formatacao(dia, mes, "Libra")
+    elif dia>=23 and dia<=31 and mes==m[7] or dia>=1 and dia<=21 and mes==m[8]:
+        formatacao(dia, mes, "Escorpião")
+    elif dia>=22 and dia<=30 and mes==m[8] or dia>=1 and dia<=21 and mes==m[9]:
+        formatacao(dia, mes, "Sagitario")
+    elif dia>=21 and dia<=31 and mes==m[9] or dia>=1 and dia<=19 and mes==m[10]:
+        formatacao(dia, mes, "Capricornio")
+    elif dia>=20 and dia<=31 and mes==m[10] or dia>=1 and dia<=18 and mes==m[11]:
+        formatacao(dia, mes, "Aquario")
+    elif dia>=19 and dia<=29 and mes==m[11] or dia>=1 and dia<=19 and mes==m[0]:
+        formatacao(dia, mes, "Peixes")
 
 if __name__ == "__main__":
-    nascimento()
+    main()
